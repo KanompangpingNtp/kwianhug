9b9bc07595046ec4ec97c71eebe096091b961333
Aoy Natthaphong <nath.wangsungnone@gmail.com>
2025-03-19 16:59:32 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,11 +7,26 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+
+    <style>
+        body {
+
+            background-image: url('{{ asset('login/BG6.png') }}');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            min-height: 100vh;
+            padding: 2rem 0px;
+
+        }
+
+    </style>
+
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-6 col-lg-4">
                 <div class="card shadow-lg">
-                    <div class="card-header text-white text-center py-4" style="background-color: blue;">
+                    <div class="card-header text-white text-center py-4" style="background-color: rgb(8,124,252);">
                         <h4 class="mb-2">สำหรับผู้ดูแลระบบ</h4>
                         <h6>สำนักงานเทศบาลตำบลเกวียนหัก</h6>
                     </div>
@@ -27,27 +42,29 @@
                         @endif
                         <form action="{{route('Login')}}" method="POST">
                             @csrf
+
                             <div class="mb-3">
                                 <label for="email" class="form-label">อีเมล</label>
                                 <input type="email" name="email" id="email" class="form-control" required>
                             </div>
+
                             <div class="mb-3">
                                 <label for="password" class="form-label">รหัสผ่าน</label>
                                 <input type="password" name="password" id="password" class="form-control" required>
                             </div>
+
                             <div class="d-grid gap-2">
-                                <button type="submit" class="btn" style="background-color: blue; color: white;">เข้าสู่ระบบ</button>
+                                <button type="submit" class="btn" style="background-color: rgb(8,124,252); color:white;">เข้าสู่ระบบ</button>
                             </div>
+
                         </form>
                     </div>
-                    {{-- <div class="card-footer text-center py-3">
-                        <p class="mb-0">ยังไม่มีบัญชี? <a href="{{ route('showRegistrationForm') }}" class="text-decoration-none">สมัครสมาชิก</a></p>
-                </div> --}}
+                </div>
             </div>
         </div>
     </div>
-    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 </html>
