936f52284313c85441dc869aba54873c688bf9d6
Aoy Natthaphong <nath.wangsungnone@gmail.com>
2025-03-20 11:53:34 +0700

    update

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -1,61 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
     <title>@yield('title') | Dashboard </title>
+    <link href="{{asset('dashboard/css/styles.css')}}" rel="stylesheet" />
 
-    <!-- Bootstrap (จาก Gentelella) -->
-    <link href="{{ asset('gentelella/vendors/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
-
-    <!-- Font Awesome (จาก Gentelella) -->
-    <link href="{{ asset('gentelella/vendors/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
-
-    <!-- Custom Theme Style (จาก Gentelella) -->
-    <link href="{{ asset('gentelella/build/css/custom.min.css') }}" rel="stylesheet">
-
-    {{-- font-awesome --}}
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" rel="stylesheet">
-
-    {{-- bootstrap 5 --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-    {{-- datatable --}}
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css">
-
-    {{-- bootstrap icons --}}
+    {{-- bootstrap icon --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
-    {{-- google font --}}
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+    {{-- datatables --}}
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css">
 
     {{-- sweetalert2 --}}
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    {{-- font-awesome --}}
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" rel="stylesheet">
 </head>
-
-<style>
-    .container,
-    .container-lg,
-    .container-md,
-    .container-sm,
-    .container-xl,
-    .container-xxl {
-        max-width: 100% !important;
-        width: 100% !important;
-        padding-left: 0 !important;
-        padding-right: 0 !important;
-    }
-
-    body {
-        font-family: 'Bai Jamjuree', sans-serif;
-    }
-
-</style>
-
-<body class="nav-md">
-
+<body>
+    <style>
+        body {
+            font-size: 15px;
+        }
+    </style>
     @if ($message = Session::get('success'))
     <script>
         Swal.fire({
@@ -65,32 +36,119 @@
 
     </script>
     @endif
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+        <!-- Navbar Brand-->
+        <a class="navbar-brand ps-3">Admin Dashbord</a>
+        <!-- Sidebar Toggle-->
+        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!" title="Toggle sidebar">
+            <i class="fas fa-bars"></i>
+        </button>
+        <!-- Navbar Search-->
+        <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0"></form>
+        <!-- Navbar-->
+        <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    {{-- <li><a class="dropdown-item" href="#!">Settings</a></li>
+                    <li><a class="dropdown-item" href="#!">Activity Log</a></li>
+                    <li>
+                        <hr class="dropdown-divider" />
+                    </li> --}}
+                    <li>
+                        <form action="{{ route('logout') }}" method="POST" style="display: inline;">
+                            @csrf
+                            <button type="submit" class="dropdown-item">
+                                ออกจากระบบ<i class="bi bi-door-closed-fill ms-3"></i>
+                            </button>
+                        </form>
+                    </li>
 
-    <div class="container body">
-        <div class="main_container">
-            @include('admin.layouts.sidebar')
-            @include('admin.layouts.topnav')
-
-            <!-- page content -->
-            <div class="right_col" role="main">
-                @yield('content')
-            </div>
-            <!-- /page content -->
-
-            @include('admin.layouts.footer')
+                </ul>
+            </li>
+        </ul>
+    </nav>
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+                        <div class="sb-sidenav-menu-heading">MENU</div>
+                        <a class="nav-link" href="{{route('NoticeBoardHome')}}">
+                            <div class="sb-nav-link-icon">
+                                <i class="fa-solid fa-database"></i>
+                            </div>
+                            ป้ายประกาศ
+                        </a>
+                        <a class="nav-link" href="{{route('AwardsofPrideHome')}}">
+                            <div class="sb-nav-link-icon">
+                                <i class="fa-solid fa-database"></i>
+                            </div>
+                            รางวัลแห่งความภาคภูมิใจ
+                        </a>
+                        <a class="nav-link" href="{{route('ActivityHome')}}">
+                            <div class="sb-nav-link-icon">
+                                <i class="fa-solid fa-database"></i>
+                            </div>
+                            กิจกรรม
+                        </a>
+                        <a class="nav-link" href="{{route('PressReleaseHome')}}">
+                            <div class="sb-nav-link-icon">
+                                <i class="fa-solid fa-database"></i>
+                            </div>
+                            ข่าวประชาสัมพันธ์
+                        </a>
+                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts1" aria-expanded="false" aria-controls="collapseLayouts1">
+                            <div class="sb-nav-link-icon">
+                                <i class="fa-solid fa-folder"></i>
+                            </div>
+                            การประกาศ
+                            <div class="sb-sidenav-collapse-arrow">
+                                <i class="fas fa-angle-down"></i>
+                            </div>
+                        </a>
+                        <div class="collapse" id="collapseLayouts1" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                            <nav class="sb-sidenav-menu-nested nav">
+                                <a class="nav-link" href="{{route('ProcurementHome')}}">ประกาศจัดซื้อจัดจ้าง</a>
+                                <a class="nav-link" href="{{route('ProcurementResultsHome')}}">ผลประกาศจัดซื้อจัดจ้าง</a>
+                                <a class="nav-link" href="{{route('AveragePriceHome')}}">ประกาศราคากลาง</a>
+                                <a class="nav-link" href="{{route('ProcurementReportHome')}}">รายงานผลจัดซื้อจัดจ้าง</a>
+                            </nav>
+                        </div>
+                        <a class="nav-link" href="{{route('TouristAttractionPage')}}">
+                            <div class="sb-nav-link-icon">
+                                <i class="fa-solid fa-database"></i>
+                            </div>
+                            แนะนำสถานที่ท่องเที่ยว
+                        </a>
+                        <a class="nav-link" href="{{route('ManagePersonnel')}}">
+                            <div class="sb-nav-link-icon">
+                                <i class="fa-solid fa-database"></i>
+                            </div>
+                            บุคลากร
+                        </a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid px-4">
+                    <div class="mt-4 text-center"></div>
+                    @yield('content')
+                    <br>
+                </div>
+            </main>
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid px-4">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <div class="text-muted">Copyright &copy; บริษัท GM SKY สงวนสิทธิ์ 2024</div>
+                    </div>
+                </div>
+            </footer>
         </div>
     </div>
-
-    <script src="{{ asset('gentelella/vendors/jquery/dist/jquery.min.js') }}"></script>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
-    <!-- Bootstrap JS (จาก Gentelella) -->
-    <script src="{{ asset('gentelella/vendors/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-
-    <!-- Custom Theme Scripts (จาก Gentelella) -->
-    <script src="{{ asset('gentelella/build/js/custom.min.js') }}"></script>
-
-    @stack('scripts')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="{{asset('dashboard/js/scripts.js')}}"></script>
 </body>
 </html>
