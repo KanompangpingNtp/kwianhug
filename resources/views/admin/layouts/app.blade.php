2002c81cd54337e43799419ebaf329e18be69a0d
Aoy Natthaphong <nath.wangsungnone@gmail.com>
2025-03-20 15:27:25 +0700

    update

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -20,12 +20,20 @@
 
     {{-- font-awesome --}}
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" rel="stylesheet">
+
+    {{-- google font --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
 </head>
 <body>
     <style>
         body {
-            font-size: 15px;
+            font-size: 13px;
+            font-family: 'Bai Jamjuree', sans-serif;
         }
+
     </style>
     @if ($message = Session::get('success'))
     <script>
