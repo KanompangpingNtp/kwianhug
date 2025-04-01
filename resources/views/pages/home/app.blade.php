dcabedc6ccc65e632537c0dd327db8cdc5649e59
Aoy Natthaphong <nath.wangsungnone@gmail.com>
2025-04-01 22:58:15 +0700

    update

--- resources/views/pages/home/app.blade.php
+++ resources/views/pages/home/app.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title', 'เกวียนหัก')
+@section('title', 'เทศบาลตำบลเกวียนหัก')
 @section('content')
     @include('pages.home.components.section-1')
     @include('pages.home.components.section-2')
