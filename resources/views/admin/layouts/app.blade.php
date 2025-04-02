2282cde96e4eea0fec9eb218843cef8ccb5c8b67
Aoy Natthaphong <nath.wangsungnone@gmail.com>
2025-04-02 08:51:26 +0700

    update

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -48,7 +48,7 @@
     @endif
     <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
         <!-- Navbar Brand-->
-        <a class="navbar-brand ps-3"> <i class="fa-solid fa-database me-3"></i>ระบบจัดการข้อมูล</a>
+        <a class="navbar-brand ps-3" href="{{route('AdminIndex')}}"> <i class="fa-solid fa-database me-3"></i>ระบบจัดการข้อมูล</a>
         <!-- Sidebar Toggle-->
         <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!" title="Toggle sidebar">
             <i class="fas fa-bars"></i>
