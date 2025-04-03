a5c274c5f030d4160903e1cbb2c29d80839571c2
Aoy Natthaphong <nath.wangsungnone@gmail.com>
2025-04-03 13:58:00 +0700

    ีupdate

--- resources/views/layouts/sub-layout.blade.php
+++ resources/views/layouts/sub-layout.blade.php
@@ -507,8 +507,10 @@
                                         <div class="navbar-text">เมนูสำหรับประชาชน</div>
                                     </a>
                                     <ul class="dropdown-menu" aria-labelledby="citizenMenuDropdown">
-                                        <li><a class="dropdown-item" href="#">รับเรื่องราวร้องทุกข์ </a></li>
-                                        <li><a class="dropdown-item" href="#">รับแจ้งร้องเรียนทุจริตประพฤติมิชอบ</a>
+                                        <li><a class="dropdown-item" href="{{ route('ReceiveComplaintsForm') }}">รับเรื่องราวร้องทุกข์
+                                            </a>
+                                        </li>
+                                        <li><a class="dropdown-item" href="{{ route('SatisfactionForm') }}">รับแจ้งร้องเรียนทุจริตประพฤติมิชอบ</a>
                                         </li>
                                         <li><a class="dropdown-item" href="#">แบบสอบถามความพึงพอใจ </a></li>
                                         @foreach ($PublicMenus as $detail)
