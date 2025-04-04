97728397b9d244c2bb2d58194604dd86d8914786
Kraprow <kamalat.g@nsru.ac.th>
2025-04-04 17:13:40 +0700

    update

--- resources/views/pages/home/components/video-top.blade.php
+++ resources/views/pages/home/components/video-top.blade.php
@@ -1,4 +1,4 @@
 <video width="100%" autoplay loop muted playsinline style="object-fit: cover; margin:0px; padding:0px; display: block;">
-    <source src="{{ asset('pages/home/video/อนิเมชัีนเกวียนหักส่วนบน.webm') }}" type="video/mp4">
+    <source src="{{ asset('pages/home/video/อนิเมชัีนเกวียนหักส่วนบนเเก้.webm') }}" type="video/mp4">
     Your browser does not support the video tag.
 </video>
