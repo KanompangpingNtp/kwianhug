8fa18f99ee25e878f14473f21a66dabdbed4b36a
Kraprow <kamalat.g@nsru.ac.th>
2025-04-09 09:32:50 +0700

    update

--- resources/views/pages/home/components/map.blade.php
+++ resources/views/pages/home/components/map.blade.php
@@ -1,7 +1,7 @@
 <head>
     <style>
         .bg-page-map {
-            background-image: url('{{ asset('pages/home/map/map-last-version.png') }}');
+            background-image: url('{{ asset('pages/home/map/map-very-very-version.png') }}');
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
